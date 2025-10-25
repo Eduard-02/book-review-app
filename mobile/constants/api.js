58cacffa5f5dc8724c34451d75dd1798182a1334
Eduard-02 <eduardocebola83@gmail.com>
2025-10-25 17:05:57 +0300

    Added frontend/mobile folder

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,1 +1,13 @@
-export const API_URL = "http://localhost:3000/api";
+import { Platform } from "react-native";
+import Constants from "expo-constants";
+
+const extra = Constants?.expoConfig?.extra ?? Constants?.manifest?.extra;
+const configuredUrl = extra?.apiUrl ?? process.env.EXPO_PUBLIC_API_URL;
+
+const localUrl = Platform.select({
+  android: "http://10.0.2.2:5000",
+  default: "http://localhost:5000",
+});
+
+export const API_BASE_URL = (configuredUrl || localUrl).replace(/\/$/, "");
+export const API_URL = `${API_BASE_URL}/api`;
